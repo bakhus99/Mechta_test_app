bc21b8b81850c57254f471b24850bdeeacb2be78
bakhus <tytenov99@gmail.com>
2024-05-16 15:40:17 +0500

    fixes

--- core/src/main/java/kz/bakhus/core/components/AddToFavButton.kt
+++ core/src/main/java/kz/bakhus/core/components/AddToFavButton.kt
@@ -1,12 +1,9 @@
 package kz.bakhus.core.components
 
-import androidx.compose.foundation.layout.FlowColumnScopeInstance.align
-import androidx.compose.foundation.layout.FlowRowScopeInstance.align
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
